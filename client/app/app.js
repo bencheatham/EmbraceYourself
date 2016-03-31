e34ad0073cba91d29b6cd2673785cbedb2965257
Alex Paczynski <apaczy@gmail.com>
2016-03-31 11:08:41 -0700

    Installed Bower and saved angular & angular-route as dependencies, updated angular skeleton, added index.html and a css stylesheet

--- client/app/app.js
+++ client/app/app.js
@@ -1,35 +1,41 @@
+// Sets up main angular module and dependency injections
 angular.module('ridehook', [
   'ridehook.trips',
   'ridehook.messages',
   'ridehook.auth',
   'ngRoute'
  ]) 
- .config(function($routeProvider, $httpProvider) {
-   $routeProvider
-     .when('/signin', {
-       templateUrl: 'app/auth/signin.html',
-       controller: 'AuthController'
-     })
-     .when('/signup', {
-       templateUrl: 'app/auth/signup.html',
-       controller: 'AuthController'
-     })
-     .when('/trips', {
-       templateUrl: 'app/trips/trips.html',
-       controller: 'TripsController'
-       authenticate: true,
-     })
-     .when('/messages', {
-       templateUrl: 'app/messages/messages.html',
-       controller: 'MessagesController'
-       authenticate: true,
-     })
-     .otherwise({
-       redirectTo: '/'
-     })
+// Angular's within-the-page routing system (uses 'angular-route')
+.config(function($routeProvider, $httpProvider) {
+  $routeProvider
+  //Each route binds a view (.html) and a controller to an endpoint (/signin)
+    .when('/signin', {
+     templateUrl: 'app/auth/signin.html',
+     controller: 'AuthController'
+    })
+    .when('/signup', {
+     templateUrl: 'app/auth/signup.html',
+     controller: 'AuthController'
+    })
+    .when('/trips', {
+     templateUrl: 'app/trips/trips.html',
+     controller: 'TripsController'
+     authenticate: true,
+    })
+    .when('/messages', {
+     templateUrl: 'app/messages/messages.html',
+     controller: 'MessagesController'
+     authenticate: true,
+    })
+    .otherwise({
+     redirectTo: '/'
+    });
 
-    // $httpProvider.interceptors.push('AttachTokens');
  })
- // .factory('AttachTokens', function($window) {
+// .run(function ($rootScope, $location) {
 
- // })
+// })
+.controller('main', function ($scope) {
+
+
+});
