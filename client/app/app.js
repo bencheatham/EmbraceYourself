c20d9110b93e6c40a9a2d46973a846ffe52decfa
Sigmund Tang <shindigira@gmail.com>
2016-03-31 17:22:49 -0700

    Revert "Client side set-up"

--- client/app/app.js
+++ client/app/app.js
@@ -1,44 +1,35 @@
-// Sets up main angular module and dependency injections
 angular.module('ridehook', [
   'ridehook.trips',
   'ridehook.messages',
   'ridehook.auth',
   'ngRoute'
  ]) 
-// Angular's within-the-page routing system (uses 'angular-route')
-.config(function($routeProvider, $httpProvider) {
-  $routeProvider
-  //Each route binds a view (.html) and a controller to an endpoint (/signin)
-    .when('/signin', {
-     templateUrl: 'app/auth/signin.html',
-     controller: 'AuthController'
-    })
-    .when('/signup', {
-     templateUrl: 'app/auth/signup.html',
-     controller: 'AuthController'
-    })
-    .when('/trips', {
-     templateUrl: 'app/trips/trips.html',
-     controller: 'TripsController'
-     authenticate: true,
-    })
-    .when('/messages', {
-     templateUrl: 'app/messages/messages.html',
-     controller: 'MessagesController'
-     authenticate: true,
-    })
-    .otherwise({
-     redirectTo: '/'
-    });
+ .config(function($routeProvider, $httpProvider) {
+   $routeProvider
+     .when('/signin', {
+       templateUrl: 'app/auth/signin.html',
+       controller: 'AuthController'
+     })
+     .when('/signup', {
+       templateUrl: 'app/auth/signup.html',
+       controller: 'AuthController'
+     })
+     .when('/trips', {
+       templateUrl: 'app/trips/trips.html',
+       controller: 'TripsController'
+       authenticate: true,
+     })
+     .when('/messages', {
+       templateUrl: 'app/messages/messages.html',
+       controller: 'MessagesController'
+       authenticate: true,
+     })
+     .otherwise({
+       redirectTo: '/'
+     })
 
+    // $httpProvider.interceptors.push('AttachTokens');
  })
-// .run(function ($rootScope, $location) {
+ // .factory('AttachTokens', function($window) {
 
-// })
-.controller('main', function ($scope) {
-  
-
-})
-.factory('M', function ($http) {
-
-});
+ // })
