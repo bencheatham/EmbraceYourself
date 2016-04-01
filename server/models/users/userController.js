869b44a79cde9f8b383bb305e6ff2d12884b511f
shindigira <shindigira@gmail.com>
2016-04-01 14:19:17 -0700

    working get and post communications with postgres db using node pg

--- server/models/users/userController.js
+++ server/models/users/userController.js
@@ -1,68 +1,134 @@
-var User = require('./userModel.js'),
+function tableSure (client) {
 
-/*exmaple of how to make table relatonsips:
-var User = sequelize.define('user', { name: Sequelize.STRING })
-  , Task = sequelize.define('task', { name: Sequelize.STRING })
-  , Tool = sequelize.define('tool', { name: Sequelize.STRING })
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    var query1 = client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255))');
 
-Task.belongsTo(User)
-User.hasMany(Task)
-User.hasMany(Tool, { as: 'Instruments' })
+    var query2 = client.query('CREATE TABLE IF NOT EXISTS cars(id SERIAL PRIMARY KEY, text VARCHAR(255))');
+
+    // var query3 = client.query("INSERT INTO users (firstname, lastname) VALUES ('Bob', 'Barley')");
 
 
-sequelize.sync().then(function() {
-  // this is where we continue ...
-})
-*/
+    query2.on('end', function() { client.end(); });
+    });
 
+
+}
+
+function getUsers (req, res, client) {
+  var results = [];
+  client.connect(function(err) {
+    if(err) {
+      console.error('get failed!');
+      return res.status(500).json({ success: false, data: err});
+    }
+
+    var query = client.query('SELECT * FROM users');
+
+    query.on('row', function(row) {
+      results.push(row);
+    });
+
+    query.on('end', function() {
+      client.end();
+      return res.json(results);
+    });
+  }); // end client.connect
+}
+
+function newUser (firstname, lastname, req, res, client) {
+
+  client.connect(function(err) {
+    if(err) {
+      console.error('post failed!');
+      return res.status(500).json({ success: false, data: err});
+    }
+
+    var query = client.query("INSERT INTO users(firstname, lastname) values($1, $2)", [firstname, lastname]);
+
+
+    query.on('end', function() {
+      client.end();
+      return res.status(302);
+    });
+
+  }); // end client.connect
+}
 
 module.exports = {
-
-
-  findAllUsers: function(req, res, next) {
-    
-    User.findAll({ }).then(function(users) {
-      console.log(JSON.stringify(tasks));
-    });
-  },
-
-
-  signup: function(req, res, next) {
-
-    User
-      .findOrCreate({where: {username: 'sdepold'}, defaults: {job: 'Technical Lead JavaScript'}})
-      .spread(function(user, created) {
-         console.log(user.get({
-         plain: true
-         }))
-         console.log(created)
-
-    /*EXAMPLE USER GOES HERE:
-      {
-        username: 'sdepold',
-        job: 'Technical Lead JavaScript',
-        id: 1,
-        createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
-        updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
-      }
-      created: true
-    */
-      });
-  },
-
-
-  signin: function(req, res, next) {
-
-    //code goes here
-
-  },
-
-
-  checkAuth: function(req, res, next) {
-
-    //code goes here
-
-  }
-
-
+  tableSure: tableSure,
+  getUsers: getUsers,
+  newUser: newUser
 };
+
+
+// var User = require('./userModel.js'),
+//
+// /*exmaple of how to make table relatonsips:
+// var User = sequelize.define('user', { name: Sequelize.STRING })
+//   , Task = sequelize.define('task', { name: Sequelize.STRING })
+//   , Tool = sequelize.define('tool', { name: Sequelize.STRING })
+//
+// Task.belongsTo(User)
+// User.hasMany(Task)
+// User.hasMany(Tool, { as: 'Instruments' })
+//
+//
+// sequelize.sync().then(function() {
+//   // this is where we continue ...
+// })
+// */
+//
+//
+// module.exports = {
+//
+//
+//   findAllUsers: function(req, res, next) {
+//
+//     User.findAll({ }).then(function(users) {
+//       console.log(JSON.stringify(tasks));
+//     });
+//   },
+//
+//
+//   signup: function(req, res, next) {
+//
+//     User
+//       .findOrCreate({where: {username: 'sdepold'}, defaults: {job: 'Technical Lead JavaScript'}})
+//       .spread(function(user, created) {
+//          console.log(user.get({
+//          plain: true
+//          }))
+//          console.log(created)
+//
+//     /*EXAMPLE USER GOES HERE:
+//       {
+//         username: 'sdepold',
+//         job: 'Technical Lead JavaScript',
+//         id: 1,
+//         createdAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET),
+//         updatedAt: Fri Mar 22 2013 21: 28: 34 GMT + 0100(CET)
+//       }
+//       created: true
+//     */
+//       });
+//   },
+//
+//
+//   signin: function(req, res, next) {
+//
+//     //code goes here
+//
+//   },
+//
+//
+//   checkAuth: function(req, res, next) {
+//
+//     //code goes here
+//
+//   }
+//
+//
+// };
