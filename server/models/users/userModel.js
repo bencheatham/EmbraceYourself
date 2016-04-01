bc480aae0d3224096c186e62b1efc25aa1758992
shindigira <shindigira@gmail.com>
2016-04-01 14:19:17 -0700

    working get and post communications with postgres db using node pg

--- server/models/users/userModel.js
+++ server/models/users/userModel.js
@@ -1,19 +1,29 @@
-var User = sequelize.define('user', {
-  firstName: {
-    type: Sequelize.STRING,
-    field: 'first_name' // Will result in an attribute that is firstName when user facing but first_name in the database
-  },
-  lastName: {
-    type: Sequelize.STRING
-  }
-}, {
-  freezeTableName: true // Model tableName will be the same as the model name
-});
+// PostgresQL database Setup
+// Official Documentation: https://github.com/brianc/node-postgres/wiki/pg
+// Guide: http://mherman.org/blog/2015/02/12/postgresql-and-nodejs/
 
-User.sync({force: true}).then(function () {
-  // Table created
-  return User.create({
-    firstName: 'John',
-    lastName: 'Hancock'
+var pg = require('pg');
+// var conString = "postgres://username:password@localhost/database";
+var connectionString = process.env.DATABASE_URL || 'postgres://localhost:5432/test';
+
+var userTableSure = function () {
+
+  var client = new pg.Client(connectionString);
+
+  client.connect(function(err) {
+    if(err) {
+      return console.error('could not connect to postgres', err);
+    }
+    var query1 = client.query('CREATE TABLE IF NOT EXISTS users(id SERIAL PRIMARY KEY, firstname VARCHAR(255), lastname VARCHAR(255))');
+
+    query1.on('end', function() { client.end(); });
   });
-});
+
+}
+
+module.exports = {
+  userTableSure: userTableSure
+};
+
+// var query2 = client.query('CREATE TABLE IF NOT EXISTS cars(id SERIAL PRIMARY KEY, text VARCHAR(255))');
+// query2.on('end', function() { client.end(); });
