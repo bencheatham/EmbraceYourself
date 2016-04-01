dae53457995ba0abf610a8ff3603524f060c1f15
Jenna Gain <gain.jenna@gmail.com>
2016-04-01 14:20:50 -0700

    Modal working

--- client/app/app.js
+++ client/app/app.js
@@ -41,6 +41,7 @@ angular.module('ridehook', [
   $scope.showTabDialog = function(ev) {
     $mdDialog.show({
       controller: DialogController,
+      templateUrl: 'tabDialog.tmpl.html',
       parent: angular.element(document.body),
       targetEvent: ev,
       clickOutsideToClose:true
