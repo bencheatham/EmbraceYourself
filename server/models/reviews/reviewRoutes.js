11fc48f3500699be38ac9b2cf5c60822ed506a96
bencheatham <ben@cheatham.me>
2016-04-05 16:18:10 -0700

    working on connecting reviews client and server sides

--- server/models/reviews/reviewRoutes.js
+++ server/models/reviews/reviewRoutes.js
@@ -3,8 +3,8 @@ var reviewController = require('./reviewController.js');
 module.exports = function (app) {
 
  console.log('IN REVIEW CONTROLLER');
- app.post('/', reviewController.addReview);
- app.get('/', reviewController.getReviews);
+ app.post('/addReview', reviewController.addReview);
+ app.get('/getUserReviews', reviewController.getReviews);
 
 
 }
