c988e9547518413f2b45c06263b261a1e201255f
Jenna Gain <gain.jenna@gmail.com>
2016-04-05 21:20:27 -0700

    Added controller and search requests

--- client/app/home/home.js
+++ client/app/home/home.js
@@ -1,5 +1,27 @@
 angular.module('ridehook.home', [])
 
-.controller('HomeController', function ($scope){
+.controller('HomeController', function ($scope, $http){
+
+
+	$scope.searchTrip = function(information){
+
+		var date = information.depart_date;
+		var day = date.getDate();
+		var monthIndex = date.getMonth() + 1;
+		var year = date.getFullYear();
+		var newDepartDate = "" + monthIndex + "/" + day + "/" + year;
+		information.depart_date = newDepartDate;
+		console.log(information)
+
+		return $http({
+			method: 'POST', 
+			url: 'data/trips/findtrip',
+			data: information
+		})
+		.then(function(resp){
+		console.log(resp);
+		return resp.results;
+		})
+	}
 
 });
