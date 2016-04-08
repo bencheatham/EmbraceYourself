6ddfd87c657a3a152f012a45bb4148ce6cc27fad
Jenna Gain <gain.jenna@gmail.com>
2016-04-08 15:22:27 -0700

    Profile CSS updates

--- client/app/profile/profile.js
+++ client/app/profile/profile.js
@@ -1,9 +1,38 @@
 
 angular.module('ridehook.profile', [])
 
-.controller('ProfileController', function ($scope, $window, $location) {
- 
- 
+.controller('ProfileController', function ($scope, $window, $location, $http) {
+	if ($window.sessionStorage.id){
+	$scope.firstname = $window.sessionStorage.fn;
+	$scope.biography = null;
+}
 
+	// $scope.showEditableProfile = function(){
+	// 	if ($window.sessionStorage.id){
+	// $scope.firstname = $window.sessionStorage.fn;
+	// $scope.biography = null;
+	// 	} else{
 
+	// 	}
+	// };
+
+	$scope.getUserDisplay = function(data, req, res, client){
+		
+		information = {
+			biography: $scope.biography,
+			id : $window.sessionStorage.id
+		}
+		return $http({
+			method: 'POST',
+			url: '/data/users/profile'	
+		})
+		.then(function(res){
+			console.log("cat")
+			console.log(resp.body)
+		});
+	};
+
+	if ($window.sessionStorage){
+		$scope.getUserDisplay();
+	}
 });
