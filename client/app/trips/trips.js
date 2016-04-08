ae77d36e479af0a80dceed1418e330c8742567cc
Alex Paczynski <apaczy@gmail.com>
2016-04-08 14:20:22 -0700

    Small change to reimpliment create a trip redirect that had been commented out

--- client/app/trips/trips.js
+++ client/app/trips/trips.js
@@ -7,21 +7,6 @@ angular.module('ridehook.trips', [])
 
   $scope.createTrip = function () {
 
-    // var date = new Date($scope.trip.startDate);
-    // var day = date.getDate();
-    // var monthIndex = date.getMonth() + 1;
-    // var year = date.getFullYear();
-    // var newDepartDate = "" + monthIndex + "/" + day + "/" + year;
-    // $scope.trip.startDate = newDepartDate;
-    // console.log($scope.trip.startDate);
-
-    // date = new Date($scope.trip.endDate);
-    // day = date.getDate();
-    // monthIndex = date.getMonth() + 1;
-    // year = date.getFullYear();
-    // var newArriveDate = "" + monthIndex + "/" + day + "/" + year;
-    // $scope.trip.endDate = newArriveDate;
-    // console.log($scope.trip.endDate);
     console.log('Start date format: ', $scope.trip.startDate.toLocaleDateString());
     console.log('End date format: ', $scope.trip.endDate.toLocaleDateString());
 
@@ -29,30 +14,13 @@ angular.module('ridehook.trips', [])
 		description: $scope.trip.description,
 		pickup_point: $scope.trip.pickup,
 		dropoff_point: $scope.trip.dropoff,
-		depart_date: $scope.trip.startDate.toLocaleDateString(),	//Date.parse($scope.trip.startDate),
+		depart_date: $scope.trip.startDate.toLocaleDateString(),
 		depart_time: $scope.trip.departHour + ':' + $scope.trip.departMinute + $scope.trip.departTimeperiod,
-		arrival_date: $scope.trip.endDate.toLocaleDateString(),	//Date.parse($scope.trip.endDate),
+		arrival_date: $scope.trip.endDate.toLocaleDateString(),
 		arrival_time: $scope.trip.arriveHour + ':' + $scope.trip.arriveMinute + $scope.trip.arriveTimeperiod,
 		seats: Number($scope.trip.seats),
-		user_id: $window.sessionStorage.id 	// will reconfig this when we can get current user id from session
+		user_id: $window.sessionStorage.id
 	};
-
-    // new trip dummy data
-
-    // var tripObj = {
-    //  description: 'Yosemite camping trip!',
-    //  pickup_point: 'San Francisco',
-    //  dropoff_point: 'Yosemite Valley',
-    //  depart_date: '1460098800000',
-    //  depart_time: '4:30PM',
-    //  arrive_date: '1460098800000',
-    //  arrive_time: '9:30PM',
-    //  seats: 4,
-    //  user_id: 1
-    // };
-
-    // console.log('seats', Number($scope.trip.seats), 'type: ', typeof Number($scope.trip.seats));
-    // console.log('tripObj to send to server: ', tripObj);
 
     return $http({
       method: 'POST',
@@ -61,8 +29,8 @@ angular.module('ridehook.trips', [])
 	})
 	.then(function (resp) {
 		console.log('Successful POST request: ', resp);
-		return resp;
-		//$location.url('/home');
+		// return resp;
+		$location.url('/home');
 	});
 
   };
