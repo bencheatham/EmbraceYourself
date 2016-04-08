47800a1d6dab4434931c002a4d10dd20bdfb5534
bencheatham <ben@cheatham.me>
2016-04-08 09:01:20 -0700

    working with trip and user flow are trips

--- server/config/middleware.js
+++ server/config/middleware.js
@@ -7,6 +7,7 @@ module.exports = function (app, express) {
   // Express 4 allows us to use multiple routers with their own configurations
   var tripRouter = express.Router();
   var reviewRouter = express.Router();
+  var userRouter = express.Router();
 
   app.use(morgan('dev'));
   app.use(bodyParser.urlencoded({extended: true}));
@@ -19,10 +20,13 @@ module.exports = function (app, express) {
   // authentication middleware used to decode token and made available on the request
   //app.use('/api/trips', helpers.decode);
   app.use('/api/reviews', reviewRouter); // user review router for review request
+  console.log('herehewerewrewrwerewrwe')
+  app.use('/api/user', userRouter);
   app.use(helpers.errorLogger);
   app.use(helpers.errorHandler);
 
   // inject our routers into their respective route files
   require('../models/trips/tripRoutes.js')(tripRouter);
   require('../models/reviews/reviewRoutes.js')(reviewRouter);
+  require('../models/users/userRoutes.js')(userRouter);
 };
