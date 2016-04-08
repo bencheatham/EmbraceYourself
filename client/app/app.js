db6f0ba9aa84e2244d91ce9c05c71f648e8e2f69
shindigira <shindigira@gmail.com>
2016-04-08 01:53:12 -0700

    logout and user tracking working

--- client/app/app.js
+++ client/app/app.js
@@ -1,209 +1,221 @@
 // Sets up main angular module and dependency injections
 angular.module('ridehook', [
-  'ridehook.trips',
-  'ridehook.messages',
-  'ridehook.home',
-  'ridehook.reviews',
-  'ridehook.tripview',
-  //'ridehook.auth',
-  'ridehook.search',
-  'ngRoute',
-  'ngMaterial'
- ])
-// Angular's within-the-page routing system (uses 'angular-route')
-.factory('authInterceptor', function ($rootScope, $q, $window) {
-  return {
-    request: function (config) {
-      config.headers = config.headers || {};
-      if ($window.sessionStorage.token) {
-        config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
-      }
-      return config;
-    },
-    response: function (response) {
-      if (response.status === 401) {
-        // handle the case where the user is not authenticated
-      }
-      return response || $q.when(response);
-    }
-  };
-})
-.config(function($routeProvider, $httpProvider) {
-  $routeProvider
-  //Each route binds a view (.html) and a controller to an endpoint (/signin)
-    .when('/signin', {
-     templateUrl: 'app/auth/signin.html',
-     controller: 'AuthController',
-     authenticate: true
+        'ridehook.trips',
+        'ridehook.messages',
+        'ridehook.home',
+        'ridehook.reviews',
+        'ridehook.tripview',
+        //'ridehook.auth',
+        'ridehook.search',
+        'ngRoute',
+        'ngMaterial'
+    ])
+    // Angular's within-the-page routing system (uses 'angular-route')
+    .factory('authInterceptor', function($rootScope, $q, $window) {
+        return {
+            request: function(config) {
+                config.headers = config.headers || {};
+                if ($window.sessionStorage.token) {
+                    config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
+                }
+                return config;
+            },
+            response: function(response) {
+                if (response.status === 401) {
+                    // handle the case where the user is not authenticated
+                }
+                return response || $q.when(response);
+            }
+        };
     })
-    .when('/signup', {
-     templateUrl: 'app/auth/signup.html',
-     controller: 'AuthController',
-     authenticate: true
-    })
-    .when('/trips', {
-     templateUrl: 'app/trips/trips.html',
-     controller: 'TripsController',
-     authenticate: true
-    })
-    .when('/viewtrip', {
-     templateUrl: 'app/trips/viewtrip.html',
-     controller: 'ViewTripController',
-     authenticate: true
-    })
-    .when('/addreview', {
-      templateUrl: 'app/reviews/addreview.html',
-      controller: 'ReviewController',
-      authenticate: true
-    })
-    .when('/userreviews', {
-      templateUrl: 'app/reviews/userreviews.html',
-      controller: 'ReviewController',
-      authenticate: true
-    })
-    .when('/search', {
-     templateUrl: 'app/search/search.html',
-     controller: 'SearchController',
-     authenticate: true
-    })
-    .when('/messages', {
-     templateUrl: 'app/messages/messages.html',
-     controller: 'MessagesController',
-     authenticate: true
-    })
-    .when('/home', {
-      templateUrl: 'app/home/home.html',
-      controller: 'HomeController',
-      authenticate: true
-    })
-    .when('/search', {
-      templateUrl: 'app/search/search.html',
-      controller: 'SearchController',
-      authenticate: false
-    })
-    .otherwise({
-     redirectTo: '/'
-    });
+    .config(function($routeProvider, $httpProvider) {
+        $routeProvider
+        //Each route binds a view (.html) and a controller to an endpoint (/signin)
+            .when('/signin', {
+                templateUrl: 'app/auth/signin.html',
+                controller: 'AuthController',
+                authenticate: true
+            })
+            .when('/signup', {
+                templateUrl: 'app/auth/signup.html',
+                controller: 'AuthController',
+                authenticate: true
+            })
+            .when('/trips', {
+                templateUrl: 'app/trips/trips.html',
+                controller: 'TripsController',
+                authenticate: true
+            })
+            .when('/viewtrip', {
+                templateUrl: 'app/trips/viewtrip.html',
+                controller: 'ViewTripController',
+                authenticate: true
+            })
+            .when('/addreview', {
+                templateUrl: 'app/reviews/addreview.html',
+                controller: 'ReviewController',
+                authenticate: true
+            })
+            .when('/userreviews', {
+                templateUrl: 'app/reviews/userreviews.html',
+                controller: 'ReviewController',
+                authenticate: true
+            })
+            .when('/search', {
+                templateUrl: 'app/search/search.html',
+                controller: 'SearchController',
+                authenticate: true
+            })
+            .when('/messages', {
+                templateUrl: 'app/messages/messages.html',
+                controller: 'MessagesController',
+                authenticate: true
+            })
+            .when('/home', {
+                templateUrl: 'app/home/home.html',
+                controller: 'HomeController',
+                authenticate: true
+            })
+            .when('/search', {
+                templateUrl: 'app/search/search.html',
+                controller: 'SearchController',
+                authenticate: false
+            })
+            .otherwise({
+                redirectTo: '/home'
+            });
 
-  $httpProvider.interceptors.push('authInterceptor');
+        $httpProvider.interceptors.push('authInterceptor');
 
- })
-// .run(function ($rootScope, $location) {
+    })
+    // .run(function ($rootScope, $location) {
 
 // })
 
-.factory('searchResults', function(){
- var sObj = {
-  results: []
- }
- return sObj;
+.factory('searchResults', function() {
+    var sObj = {
+        results: []
+    }
+    return sObj;
 })
 
-.factory('tripIDFactory', function(){
- var tObj = {
-  tripID: null
- }
- return tObj;
+.factory('tripIDFactory', function() {
+    var tObj = {
+        tripID: null
+    }
+    return tObj;
 })
 
-.controller('AppCtrl', function ($scope, $mdDialog ) {
+.controller('AppCtrl', function($scope, $mdDialog, $window, $location) {
 
-  $scope.showSignUp = function(ev) {
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: 'tabDialog.tmpl.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:true
-    })
-  };
+    $scope.loggedIn = $window.sessionStorage.un;
 
-  $scope.showSignIn = function(ev) {
-    $mdDialog.show({
-      controller: DialogController,
-      templateUrl: 'tabDialog1.tmpl.html',
-      parent: angular.element(document.body),
-      targetEvent: ev,
-      clickOutsideToClose:true
-    })
-  };
-
-  function DialogController($scope, $mdDialog, $http, $window) {
-
-    $scope.hide = function() {
-      $mdDialog.hide();
+    $scope.logOut = function () {
+      delete $window.sessionStorage.token;
+      delete $window.sessionStorage.id;
+      delete $window.sessionStorage.un;
+      delete $window.sessionStorage.fn;
+      delete $window.sessionStorage.ln;
+      $window.location.reload();
     };
 
-    $scope.cancel = function() {
-      $mdDialog.cancel();
+    $scope.showSignUp = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'tabDialog.tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+        })
     };
 
-    $scope.newUser = function(information) {
-
-      information = {
-        username: information.username,
-        password: information.password,
-        first_name: information.first_name,
-        last_name: information.last_name,
-        email: information.email,
-        age: 27,
-        profile_pic: null,
-        city: information.city,
-        state: information.state,
-        zip_code: Number(information.zip)
-      };
-
-      if(!information.username || !information.password){
-        console.log('Error: a required field is empty.');
-        return;
-      }
-
-      console.log('info obj to POST to server: ', information);
-
-      return $http({
-        method: 'POST',
-        url: '/data/users/signup',
-        data: information
-      }).then(function (response){
-        console.log(response.data);
-        $mdDialog.hide(information);
-      }, function (error) {
-        console.log(error);
-      });
+    $scope.showSignIn = function(ev) {
+        $mdDialog.show({
+            controller: DialogController,
+            templateUrl: 'tabDialog1.tmpl.html',
+            parent: angular.element(document.body),
+            targetEvent: ev,
+            clickOutsideToClose: true
+        })
     };
 
-    $scope.loginUser = function(information){
+    function DialogController($scope, $mdDialog, $http, $window, $location) {
 
-      information = {
-        username: information.username,
-        password: information.password,
-      };
+        $scope.hide = function() {
+            $mdDialog.hide();
+        };
 
-      return $http({
-        method: 'POST',
-        url: '/data/users/login',
-        data: information
-      }).then(function (response){
+        $scope.cancel = function() {
+            $mdDialog.cancel();
+        };
 
-        $window.sessionStorage.token = response.data.token;
-        $window.sessionStorage.id = response.data.user_id;
-        $window.sessionStorage.un = response.data.username;
-        $window.sessionStorage.fn = response.data.first_name;
-        $window.sessionStorage.ln = response.data.last_name;
+        $scope.newUser = function(information) {
 
-        console.log('Success: ', response);
-        $mdDialog.hide(information);
+            information = {
+                username: information.username,
+                password: information.password,
+                first_name: information.first_name,
+                last_name: information.last_name,
+                email: information.email,
+                age: 27,
+                profile_pic: null,
+                city: information.city,
+                state: information.state,
+                zip_code: Number(information.zip)
+            };
 
-      }, function (error) {
-        delete $window.sessionStorage.token;
-        delete $window.sessionStorage.id;
-        delete $window.sessionStorage.un;
-        delete $window.sessionStorage.fn;
-        delete $window.sessionStorage.ln;
-        console.log('Error: ', error);
-      });
+            if (!information.username || !information.password) {
+                console.log('Error: a required field is empty.');
+                return;
+            }
 
-    };
-  }
+            console.log('info obj to POST to server: ', information);
+
+            return $http({
+                method: 'POST',
+                url: '/data/users/signup',
+                data: information
+            }).then(function(response) {
+                console.log(response.data);
+                $mdDialog.hide(information);
+            }, function(error) {
+                console.log(error);
+            });
+        };
+
+        $scope.loginUser = function(information) {
+
+            information = {
+                username: information.username,
+                password: information.password,
+            };
+
+            return $http({
+                method: 'POST',
+                url: '/data/users/login',
+                data: information
+            }).then(function(response) {
+
+                $window.sessionStorage.token = response.data.token;
+                $window.sessionStorage.id = response.data.user_id;
+                $window.sessionStorage.un = response.data.username;
+                $window.sessionStorage.fn = response.data.first_name;
+                $window.sessionStorage.ln = response.data.last_name;
+
+                console.log('Success: ', response);
+                $mdDialog.hide(information);
+                $window.location.reload();
+
+            }, function(error) {
+                delete $window.sessionStorage.token;
+                delete $window.sessionStorage.id;
+                delete $window.sessionStorage.un;
+                delete $window.sessionStorage.fn;
+                delete $window.sessionStorage.ln;
+                console.log('Error: ', error);
+            });
+
+        };
+    }
 
 });
